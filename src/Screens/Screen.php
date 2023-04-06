1b3edade8fca460567bfea9df1c15053043a2e08
Denis Skripchenko <denskrp90@gmail.com>
2023-04-06 10:02:07 +0300

    fix: booting

--- src/Screens/Screen.php
+++ src/Screens/Screen.php
@@ -2,42 +2,16 @@
 
 namespace Dskripchenko\OrchidExtra\Screens;
 
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Orchid\Screen\Repository;
 use Orchid\Screen\Screen as BaseScreen;
-use Throwable;
 
 abstract class Screen extends BaseScreen
 {
-    /**
-     * @param Repository $repository
-     *
-     * @return View
-     */
-    public function build(Repository $repository): View
+    public function __construct()
     {
         /** @var Request $request */
         $request = request();
         $this->booting($request->all());
-        return parent::build($repository);
-    }
-
-
-    /**
-     * @param  string  $method
-     * @param  string  $slug
-     * @return View
-     * @throws Throwable
-     */
-    public function asyncBuild(string $method, string $slug): View
-    {
-        /** @var Request $request */
-        $request = request();
-        $this->booting($request->all());
-        return parent::asyncBuild($method, $slug);
     }
 
     /**
